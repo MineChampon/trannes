256380edaab771aa46426599dbe16718677a0801
MineChampon <mine.cha.vy@gmail.com>
2020-12-22 20:46:35 +0900

    update: sleep時間を変更

--- trannes/api/views/account_create.py
+++ trannes/api/views/account_create.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 
-from ..models import Users 
-
+from ..models import *
 
 def account_create(request):
     result = 'true'
